f2ebee5bb0d10681b83a9d398d3f4ec1a22c3d2d
ASCSagar <ascsagar08@gmail.com>
2025-03-21 17:57:58 +0530

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Facebook, Twitter, Instagram, ArrowUpCircle } from "lucide-react";
-import AdUnit from "@/components/AdUnit/AdUnit";
 
 const footerLinks = {
   company: [
@@ -56,76 +55,86 @@ const Footer = () => {
   };
 
   return (
-    <div>
-      <AdUnit />
-      <footer className="bg-purple-600 text-white relative">
-        <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-10 py-12">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            <div>
-              <Link href="/" className="flex items-center space-x-2">
-                <img
-                  src="/breatheoffline.png"
-                  alt="BreatheOffline"
-                  className="h-8 w-auto"
-                />
-              </Link>
-              <p className="mt-4 text-gray-200">
-                Empowering mindful technology use for a balanced digital
-                lifestyle.
-              </p>
-              <div className="flex space-x-4 mt-6">
-                {socialLinks.map(({ name, icon: Icon, href }) => (
-                  <a
-                    key={href}
-                    href={href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-white hover:text-purple-300 transition-colors duration-200"
-                  >
-                    <Icon className="h-6 w-6" />
-                    <span className="sr-only">{name}</span>
-                  </a>
-                ))}
-              </div>
-            </div>
-            {Object.entries(footerLinks).map(([section, links], index) => (
-              <div key={index}>
-                <h3 className="text-lg font-semibold mb-4 capitalize text-white">
-                  {section}
-                </h3>
-                <ul className="space-y-2">
-                  {links.map(({ name, href }) => (
-                    <li key={href}>
-                      <Link
-                        href={href}
-                        className="text-white hover:text-purple-300 transition-colors duration-200"
-                      >
-                        {name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-          <div className="border-t border-purple-500 pt-8 mt-8 text-center">
-            <p className="text-gray-200 text-sm">
-              {currentYear
-                ? `© ${currentYear} BreatheOffline. All rights reserved.`
-                : "Loading..."}
+    <footer className="bg-purple-600 text-white relative">
+      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-10 py-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div>
+            <Link href="/" className="flex items-center space-x-2">
+              <img
+                src="/breatheoffline.png"
+                alt="BreatheOffline"
+                className="h-8 w-auto"
+              />
+            </Link>
+            <p className="mt-4 text-gray-200">
+              Empowering mindful technology use for a balanced digital
+              lifestyle.
             </p>
+            <div className="flex space-x-4 mt-6">
+              {socialLinks.map(({ name, icon: Icon, href }) => (
+                <a
+                  key={href}
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white hover:text-purple-300 transition-colors duration-200"
+                >
+                  <Icon className="h-6 w-6" />
+                  <span className="sr-only">{name}</span>
+                </a>
+              ))}
+            </div>
           </div>
+          {Object.entries(footerLinks).map(([section, links], index) => (
+            <div key={index}>
+              <h3 className="text-lg font-semibold mb-4 capitalize text-white">
+                {section}
+              </h3>
+              <ul className="space-y-2">
+                {links.map(({ name, href }) => (
+                  <li key={href}>
+                    <Link
+                      href={href}
+                      className="text-white hover:text-purple-300 transition-colors duration-200"
+                    >
+                      {name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
-        {isVisible && (
-          <button
-            onClick={scrollToTop}
-            className="fixed bottom-8 right-8 p-3 rounded-full bg-purple-600 text-white shadow-lg transition-all duration-300 hover:bg-purple-700"
-          >
-            <ArrowUpCircle className="h-6 w-6" />
-          </button>
-        )}
-      </footer>
-    </div>
+        <div className="border-t border-purple-500 pt-8 mt-8 text-center">
+          <p className="text-gray-200 text-sm">
+            {currentYear ? (
+              <div>
+                © {currentYear} BreatheOffline. All rights reserved | Design &
+                Developed By{" "}
+                <a
+                  href="https://anantsoftcomputing.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white font-semibold hover:text-gray-400 transition-colors duration-300"
+                >
+                  Anant Soft Computing
+                </a>
+              </div>
+            ) : (
+              "Loading..."
+            )}
+          </p>
+        </div>
+      </div>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-8 right-8 p-3 rounded-full bg-purple-600 text-white shadow-lg transition-all duration-300 hover:bg-purple-700"
+        >
+          <ArrowUpCircle className="h-6 w-6" />
+        </button>
+      )}
+    </footer>
   );
 };
 
