422fca96ed6fb0947af1c3772758dbe5594d5ab9
ASCSagar <ascsagar08@gmail.com>
2025-04-15 17:39:25 +0530

    Adsense-Script

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,13 +36,18 @@ export default function RootLayout({ children }) {
             `,
           }}
         ></Script>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2283636579212292"
+          crossorigin="anonymous"
+        ></Script>
         <meta
           name="google-site-verification"
           content="-vEQwCv3N-7CYJlks9qASX9zmgZ-7sbhBPfH4JKkxT4"
         ></meta>
         <meta
           name="google-adsense-account"
-          content="ca-pub-1049611485767103"
+          content="ca-pub-2283636579212292"
         ></meta>
       </head>
       <body>
